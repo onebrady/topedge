5a3f9fc15c73c101be8ea7aa10337357afca5b55
onebrady <onebrady@gmail.com>
2025-10-15 09:25:41 -0500

    add GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,21 @@ export default function RootLayout({
           <WeatherBar />
           {children}
         </Providers>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6EJD5FZC4E"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6EJD5FZC4E');
+          `}
+        </Script>
+
         <Script
           src="https://links.resultreach.com/js/form_embed.js"
           strategy="lazyOnload"
